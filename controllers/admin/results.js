197d6024bb145b287722d89d69d042a965b67942
Giorgi Garsevanishvili <George.Garsevanidi@Gmail.com>
2025-03-27 14:35:02 +0200

    results route added, delete and get controllers added for admin

---
+++ controllers/admin/results.js
@@ -0,0 +1,40 @@
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../../errors");
+const VoterModel = require("../../models/voter-model");
+const voterModel = require("../../models/voter-model");
+
+const getResults = async (req, res) => {
+  try {
+    const { id: electionId } = req.params;
+    const vote = await VoterModel.find({ electionId });
+    if (vote.length < 1) {
+      throw new NotFoundError("Results not found!");
+    }
+    const result = vote.map((item) => item.answer);
+
+    res.status(StatusCodes.OK).json({ result });
+  } catch (error) {
+    throw new BadRequestError(error);
+  }
+};
+
+const deleteResults = async (req, res) => {
+  const { id: electionId } = req.params;
+  const deletedResults = await voterModel.deleteMany({ electionId });
+
+  if (!deletedResults) {
+    throw new BadRequestError(
+      `Result for Election ID: ${electionId} not found! `
+    );
+  }
+
+  if (deletedResults.deletedCount === 0) {
+    throw new BadRequestError("Failed to delete results");
+  }
+  res.status(StatusCodes.OK).send("Results Successfully deleted!");
+};
+
+module.exports = {
+  getResults,
+  deleteResults,
+};
--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,15 @@ const QRCode = require("qrcode");
 const {getAllElection , createElection, updateElection, getOneElection, deleteElection} = require('../controllers/admin/election-controlers.js')
 const {getUser, updateUser, createUser, deleteUser} = require('../controllers/admin/user-controllers.js');
 const { generateQrCodes, getQRCodes, deleteAccessQR } = require("../controllers/admin/accessqr-controllers.js");
+const { getResults, deleteResults } = require("../controllers/admin/results.js");
 
 router.route('/election').get(getAllElection).post(createElection)
 router.route('/election/:id').get(getOneElection).patch(updateElection).delete(deleteElection)
 
 router.route('/system/users').get(getUser).post(createUser)
 router.route('/system/users/:id').patch(updateUser).delete(deleteUser)
-router.route('/election/generate-qr/:id').post(generateQrCodes).get(getQRCodes).delete(deleteAccessQR)
+router.route('/election/:id/generate-qr').post(generateQrCodes).get(getQRCodes).delete(deleteAccessQR)
+router.route('/election/:id/results').get(getResults).delete(deleteResults)
 
 
 module.exports = router
